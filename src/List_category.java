7617a995c531bf1927083f85dbb903127867d54a
Andrés <andresbravomontes89@gmail.com>
2018-10-08 15:45:57 +0200

    Clase List_category

--- src/List_category.java
+++ src/List_category.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class List_category {
-
+	
+	List<Category> list = new ArrayList();
+	
+	public void addCategory (Category name) {
+		list.add(name);
+	}
+	
+	public Category search_category(String name_category) {
+		int i = 0;
+		boolean found = false;
+		
+		while(i <= list.size()) {
+			if (name_category == list.get(i).getName()) {
+				found = true;
+				i++;
+			}
+				
+		}
+		if (found) {
+			return list.get(i);
+		}else
+			return null;
+			
+	}
+	
 }
