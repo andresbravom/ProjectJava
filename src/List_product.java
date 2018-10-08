b5a0535920355ea4340250da805acf26bad76022
Andrés <andresbravomontes89@gmail.com>
2018-10-08 15:49:45 +0200

    Clase List_product

---
+++ src/List_product.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_product {
+
+	
+	List<Product> list = new ArrayList();
+	
+	public void insert_product(Product name) {
+		list.add(name);
+	}
+	
+	public Product search_product(String name_product) {
+		int i = 0;
+		boolean found = false;
+		
+		while(i <= list.size()) {
+			if (name_product == list.get(i).getName()) {
+				found = true;
+				break;
+			}
+			i++;
+		}
+		if(found) {
+			return list.get(i);
+		}
+		else 
+			return null;
+	}
+}
